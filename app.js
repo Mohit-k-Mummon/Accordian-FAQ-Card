376fc743822c42fef614ca8d45341f788ab9da09
Mohit Mummon <mohitkumar335i@gmail.com>
2022-09-07 17:49:23 -0700

    JavaScript for toggling question font weight

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const accordian = document.getElementsByClassName('content-container');
 console.log(accordian);
+
+// Looping through the HTMLCollection to apply event Listeners
 for (let i = 0; i < accordian.length; i++) {
 	accordian[i].addEventListener('click', function () {
 		this.classList.toggle('active');
+		if (this.classList.contains('active')) {
+			this.children[0].style.fontWeight = '700';
+		} else {
+			this.children[0].style.fontWeight = '400';
+		}
 	});
 }
